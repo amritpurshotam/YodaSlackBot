9ce51d814394784fe0df3ed7a81791e2edce5a34
amrit.purshotam <amrit.purshotam@entelect.co.za>
2015-11-10 17:12:49 +0200

    Add HelloResponder

---
+++ YodaSlackBot/YodaSlackBot/Responders/HelloResponder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MargieBot.Models;
+using MargieBot.Responders;
+
+namespace YodaSlackBot.Responders
+{
+    public class HelloResponder : IResponder
+    {
+        public bool CanRespond(ResponseContext context)
+        {
+            return context.Message.MentionsBot
+                && !context.BotHasResponded
+                   && context.Message.Text.ToLower().Contains("hello");
+        }
+
+        public BotMessage GetResponse(ResponseContext context)
+        {
+            var builder = new StringBuilder();
+            builder.Append("Hello ");
+            builder.Append(context.Message.User.FormattedUserID);
+            return new BotMessage { Text = builder.ToString() };
+        }
+    }
+}
