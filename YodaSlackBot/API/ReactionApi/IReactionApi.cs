f43bd9dbe5412089a24be113a9cb66eaea0b421c
amrit.purshotam <amrit.purshotam@entelect.co.za>
2015-11-10 19:28:23 +0200

    Add Reaction Api

---
+++ YodaSlackBot/API/ReactionApi/IReactionApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.ReactionApi
+{
+    public interface IReactionApi
+    {
+        bool AddReaction(string reaction, string channel, string timestamp);
+    }
+}
---
+++ YodaSlackBot/API/ReactionApi/ReactionApi.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Flurl.Http;
+using ServiceStack.Text;
+
+namespace API.ReactionApi
+{
+    public class ReactionApi : IReactionApi
+    {
+        public bool AddReaction(string reaction, string channel, string timestamp)
+        {
+            var responseString = "https://slack.com/api/reactions.add"
+                .PostUrlEncodedAsync(
+                    new
+                    {
+                        token = ConfigurationManager.AppSettings["SlackApiKey"],
+                        name = reaction,
+                        channel,
+                        timestamp
+                    })
+                .ReceiveString()
+                .Result;
+
+            var serializer = new JsonSerializer<ReactionResponseModel>();
+            var response = serializer.DeserializeFromString(responseString);
+            return response.ok;
+        }
+    }
+}
