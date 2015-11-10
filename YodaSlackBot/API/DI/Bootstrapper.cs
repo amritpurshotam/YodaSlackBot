7609dd4b1d6deca8de990636980b7729c06300c9
amrit.purshotam <amrit.purshotam@entelect.co.za>
2015-11-10 19:17:05 +0200

    Add Api project with DI

--- YodaSlackBot/API/DI/Bootstrapper.cs
+++ YodaSlackBot/API/DI/Bootstrapper.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using API.DI;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
 
-namespace YodaSlackBot.DI
+namespace API.DI
 {
-    public static class BotRunnerBootstrapper
+    public class Bootstrapper
     {
         public static IWindsorContainer Init()
         {
-            var container = Bootstrapper.Init();
-            
-            container.Install(FromAssembly.This());
-
-            return container;
+            return new WindsorContainer()
+                .Install(FromAssembly.This()
+                );
         }
     }
 }
