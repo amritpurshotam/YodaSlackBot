76c076572dafaa0fad5fa1a576057fc9be93b6d9
amrit.purshotam <amrit.purshotam@entelect.co.za>
2016-02-21 07:31:18 +0200

    Add UserInfo api method

--- YodaSlackBot/API/UserApi/IUserApi.cs
+++ YodaSlackBot/API/UserApi/IUserApi.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace API.UserApi
+﻿namespace API.UserApi
 {
     public interface IUserApi
     {
+        UserInfoResponseModel GetUserInfo(string slackApiToken, string userSlackId);
     }
 }
--- YodaSlackBot/API/UserApi/UserApi.cs
+++ YodaSlackBot/API/UserApi/UserApi.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Flurl.Http;
+using ServiceStack.Text;
 
 namespace API.UserApi
 {
     public class UserApi : IUserApi
     {
+        public UserInfoResponseModel GetUserInfo(string slackApiToken, string userSlackId)
+        {
+            var responseString = "https://slack.com/api/users.info"
+                .PostUrlEncodedAsync(
+                    new
+                    {
+                        token = slackApiToken,
+                        user = userSlackId
+                    })
+                .ReceiveString()
+                .Result;
 
+            var serializer = new JsonSerializer<UserInfoResponseModel>();
+            var response = serializer.DeserializeFromString(responseString);
+            return response;
+        }
     }
 }
